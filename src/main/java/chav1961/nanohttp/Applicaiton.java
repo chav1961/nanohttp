13528b0d80d6fcfd8adddfe24dd07078182f8f12
chav1961 <chav1961@mail.ru>
2024-08-28 14:53:22 +0500

    Initial commit

---
+++ src/main/java/chav1961/nanohttp/Applicaiton.java
@@ -0,0 +1,10 @@
+package chav1961.nanohttp;
+
+public class Applicaiton {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module chav1961.nanohttp {
+	requires transitive chav1961.purelib;
+	requires java.base;
+	requires java.desktop;
+	requires java.datatransfer;
+	
+	exports chav1961.nanohttp;
+}
